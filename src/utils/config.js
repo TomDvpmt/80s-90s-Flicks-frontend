d6c323a139ab2a0e76777747930f3418a50ffa3f
TomDvpmt <tbsuiviactu@gmail.com>
2023-06-17 18:10:52 +0200

    Modified: API url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,6 @@
 export const TMDB_API_KEY = "2d0a75daa1b16703efb5d87960c9e67e";
 
-// export const BASE_API_URI = "https://eight0s-90s-flicks-api.onrender.com";
-export const BASE_API_URI = "http://localhost:3000";
+export const BASE_API_URI = "https://eight0s-90s-flicks-api.onrender.com";
 
 export const BASE_IMGUR_URI = "https://api.imgur.com/3/image";
 export const BASE_IMGBB_URI = "https://api.imgbb.com/1";
